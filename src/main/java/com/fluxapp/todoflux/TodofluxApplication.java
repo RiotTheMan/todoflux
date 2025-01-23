4d064694b0eac4fb300b210b9edcf26f2163c8ca
Emile <emileharelg@gmail.com>
2025-01-23 14:38:08 +0200

    Initial Commit and Minimal JWT config
    pom.xml has packages required for Spring Sec
    Package based class separation for code clarity

---
+++ src/main/java/com/fluxapp/todoflux/TodofluxApplication.java
@@ -0,0 +1,13 @@
+package com.fluxapp.todoflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodofluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodofluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fluxapp/todoflux/TodofluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fluxapp.todoflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodofluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
